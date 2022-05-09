72613520d5b5609eba23df7286d8f3b700fb14e4
Criz <crizmo595@gmail.com>
2022-05-09 19:32:09 +0530

    🧰: hd

---
+++ src/enums.rs
@@ -0,0 +1,34 @@
+enum Movement {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+fn move_avatar(m: Movement) {
+    match m {
+        Movement::Up => println!("Avatar moving up"),
+        Movement::Down => println!("Avatar moving down"),
+        Movement::Left => println!("Avatar moving left"),
+        Movement::Right => println!("Avatar moving right"),
+    }
+}
+
+pub fn run() {
+    let avatar1 = Movement::Left;
+    let avatar2 = Movement::Right;
+    move_avatar(avatar1);
+    move_avatar(avatar2);
+
+    move_avatar(Movement::Up);
+    move_avatar(Movement::Down);
+
+    // Pattern Matching
+    let x = 1;
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+}
